5ea2ec2d549de7b4c78cd5e00c398ed4463763e9
Hristiyan Dimitrov <102990696+dimitrovhris@users.noreply.github.com>
2022-04-08 11:08:55 +0300

    Create Simple5.java

---
+++ first-steps-in-coding/Simple5.java
@@ -0,0 +1,18 @@
+package simpleCalculations;
+
+import java.util.Scanner;
+
+public class Simple1 {
+
+	public static void main(String[] args) {
+		
+		Scanner readName = new Scanner (System.in);
+		String name = readName.nextLine();
+		
+		System.out.printf("Hello, %s!", name);
+	
+	
+	
+	}
+
+}
