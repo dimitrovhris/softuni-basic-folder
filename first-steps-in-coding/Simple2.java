cbe1f0aa5865e1e440b5246dc74ec3052c22065a
Hristiyan Dimitrov <102990696+dimitrovhris@users.noreply.github.com>
2022-04-08 11:06:05 +0300

    Update Simple2.java

--- first-steps-in-coding/Simple2.java
+++ first-steps-in-coding/Simple2.java
@@ -1,19 +1,9 @@
-	package simpleCalculations;
+package loops;
 
-	import java.util.Scanner;
-	
-	public class Simple2 {
-			public static void main(String[] args) {
-				
-			
-		Scanner readData = new Scanner (System.in);
-		String firstName = readData.nextLine();
-		String lastName = readData.nextLine();
-		int age = Integer.parseInt(readData.nextLine());
-		String town = readData.nextLine();
-		
-		System.out.printf("You are %s %s, a %d-years old person from %s.", firstName, lastName, age, town);
-		
-	}
-
+public class Loop1 {
+    public static void main(String[] args) {
+       for (int i = 1; i <= 10; i++){
+           System.out.println(i);
+        }
+    }
 }
