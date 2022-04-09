1f76068eaeb456fab03f2bc35c75831c9743058f
Hristiyan Dimitrov <102990696+dimitrovhris@users.noreply.github.com>
2022-04-09 12:39:13 +0300

    Create Task9.java

---
+++ first-steps-exercise/Task9.java
@@ -0,0 +1,27 @@
+package simpleCalculations;
+
+import java.util.Scanner;
+
+public class Simple8 {
+
+	public static void main(String[] args) {
+		Scanner readData = new Scanner(System.in);
+		double length = Double.parseDouble(readData.nextLine());
+		double width = Double.parseDouble(readData.nextLine());
+		double hight = Double.parseDouble(readData.nextLine());
+		double percent = Double.parseDouble(readData.nextLine());
+		
+		double volume = length * width * hight;
+		double volumeLiters = 0.001 * volume;
+		double realPercent = 0.01 * percent;
+		
+		double totalVolume = volumeLiters - realPercent * volumeLiters;
+		
+		System.out.println( totalVolume );
+		
+		
+		
+		
+		}
+
+}
