e90e5860659e3aa6f250383b8b5a29224eb0bf58
Hristiyan Dimitrov <hrisit12@gmail.com>
2022-04-11 22:50:20 +0300

    Update Task7.java

--- conditional-statements/Task7.java
+++ conditional-statements/Task7.java
@@ -1,1 +1,31 @@
 
+package conditionalStatements;
+import java.util.Scanner;
+public class Conditional7 {
+    public static void main(String[] args) {
+        Scanner readData = new Scanner(System.in);
+        String figure = readData.nextLine();
+        double area = 0.000;
+
+        if (figure.equals("square")){
+            double a = readData.nextDouble();
+            area = a * a;
+        }
+        else if(figure.equals("rectangle")){
+            double a = readData.nextDouble();
+            double b = readData.nextDouble();
+            area = a * b;
+
+        }
+        else if(figure.equals("circle")){
+            double r = readData.nextDouble();
+            area = Math.PI * Math.pow(r, 2);
+        }
+        else if(figure.equals("triangle")){
+            double a = readData.nextDouble();
+            double h = readData.nextDouble();
+            area = a * h / 2;
+        }
+        System.out.printf("%.3f", area);
+    }
+}
