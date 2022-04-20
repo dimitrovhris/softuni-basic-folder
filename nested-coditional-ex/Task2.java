8a88af3a7c62d056a5499f8f1df361aae061e288
Hristiyan Dimitrov <hrisit12@gmail.com>
2022-04-20 22:36:41 +0300

    Update Task2.java

--- nested-coditional-ex/Task2.java
+++ nested-coditional-ex/Task2.java
@@ -1,26 +1,58 @@
-package nestedConditional;
+package nestedConditionalEx;
 import java.util.Scanner;
-
-public class NestedHi {
+public class NestedEx3 {
     public static void main(String[] args) {
-        Scanner readData = new Scanner(System.in);
-        String dayOfWeek = readData.nextLine();
+        Scanner readData = new Scanner (System.in);
+        int degrees = Integer.parseInt(readData.nextLine());
+        String time = readData.nextLine();
 
-        switch (dayOfWeek) {
-            case "Monday":
-            case "Tuesday":
-            case "Wednesday":
-            case "Thursday":
-            case "Friday":
-                System.out.println("Working day");
-                break;
-            case "Saturday":
-            case "Sunday":
-                System.out.println("Weekend");
-                break;
-            default:
-                System.out.println("Error");
+        String outfit = "";
+        String shoes = "";
+
+        if (degrees >= 10 && degrees <= 18){
+            switch (time) {
+                case "Morning":
+                    outfit = "Sweatshirt";
+                    shoes = "Sneakers";
+                    break;
+                case "Afternoon":
+                case "Evening":
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                    break;
+            }
         }
+        else if (degrees > 18 && degrees <= 24){
+            switch (time){
+                case "Morning":
+                case "Evening":
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                    break;
+                case "Afternoon":
+                    outfit = "T-Shirt";
+                    shoes = "Sandals";
+                    break;
+            }
+
+        }else if( degrees >= 25){
+            switch (time){
+                case "Morning":
+                    outfit = "T-Shirt";
+                    shoes = "Sandals";
+                    break;
+                case "Afternoon" :
+                    outfit = "Swim Suit";
+                    shoes = "Barefoot";
+                    break;
+
+                case "Evening":
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                    break;
+            }
+        }
+        System.out.printf("It's %d degrees, get your %s and %s.", degrees, outfit, shoes);
 
     }
 }
