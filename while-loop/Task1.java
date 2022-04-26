37ad4a1e92dc123e7c4aab091827131a5c90f4ed
Hristiyan Dimitrov <hrisit12@gmail.com>
2022-04-26 21:56:07 +0300

    Create Task1.java

---
+++ while-loop/Task1.java
@@ -0,0 +1,13 @@
+package whileLoops;
+import java.util.Scanner;
+public class While1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input  = sc.nextLine();
+        while(!input.equals("Stop")){
+            System.out.println(input);
+            input = sc.nextLine();
+        }
+
+    }
+}
