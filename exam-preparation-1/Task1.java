6aef5ef04acd74eab420a95ea97b226b60c4c9cb
Hristiyan Dimitrov <hrisit12@gmail.com>
2022-05-10 22:17:11 +0300

    Update Task1.java

--- exam-preparation-1/Task1.java
+++ exam-preparation-1/Task1.java
@@ -1,22 +1,20 @@
-package simpleCalculationsEx;
+package exExam1;
 
 import java.util.Scanner;
 
-public class SimpleEx5 {
+public class exam11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String airCompany = sc.nextLine();
+        int adultsTickets = Integer.parseInt(sc.nextLine());
+        int kidsTickets = Integer.parseInt(sc.nextLine());
+        double adultPrice = Double.parseDouble(sc.nextLine());
+        double serviceFee = Double.parseDouble(sc.nextLine());
 
-	public static void main(String[] args) {
-
-		Scanner sc = new Scanner(System.in);
-		int penPackets = Integer.parseInt(sc.nextLine());
-		int markersPackets = Integer.parseInt(sc.nextLine());
-		int detergentLiters = Integer.parseInt(sc.nextLine());
-		double percent = Integer.parseInt(sc.nextLine());
-
-		double totalPrice = (penPackets * 5.80 + markersPackets * 7.20 + detergentLiters * 1.20);
-		double realPrice = totalPrice - (percent / 100 * totalPrice);
-		System.out.println(realPrice);
-				 
-
-	}
-
+        double kidsPrice = 0.30 * adultPrice + serviceFee;
+        adultPrice += serviceFee;
+        double totalSum = 0.20 * (adultsTickets * adultPrice + kidsTickets * kidsPrice);
+        System.out.printf("The profit of your agency from %s tickets is %.2f lv.", airCompany, totalSum);
+    }
 }
+
